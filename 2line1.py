9b9e2a39f31dd96050967e21eadd31d137e7ab12
Sean AP <66278738+2linebot@users.noreply.github.com>
2020-06-02 23:45:26 -0400

    Update 2line1.py

--- 2line1.py
+++ 2line1.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import tweepy
 import time
 import os
@@ -8,41 +7,6 @@ ckey=environ['ckey']
 csecret=environ['csecret']
 atoken=environ['atoken']
 asecret=environ['asecret']
-
-auth=tweepy.OAuthHandler(ckey, csecret)
-auth.set_access_token(atoken, asecret)
-
-api=tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
-
-user=api.me()
-
-search='Line 2 Bloor-Danforth from:TTCnotices'
-nrTweets=5
-
-for tweet in tweepy.Cursor(api.search, search).items(nrTweets):
-    try:
-        print('Alert RTd')
-        print(tweet.text)
-        tweet.retweet()
-        time.sleep(10)
-    except tweepy.TweepError as e:
-        print(e.reason)
-    except StopIteration:
-        break
-
-
-
-
-
-=======
-import tweepy
-import time
-import os
-
-ckey=os.environ['ckey']
-csecret=os.environ['csecret']
-atoken=os.environ['atoken']
-asecret=os.environ['asecret']
 
 auth=tweepy.OAuthHandler(ckey, csecret)
 auth.set_access_token(atoken, asecret)
@@ -64,9 +28,3 @@ for tweet in tweepy.Cursor(api.search, search).items(nrTweets):
         print(e.reason)
     except StopIteration:
         break
-
-
-
-
-
->>>>>>> d9e36d846ab2f51d83f8759dcf229fd1ac2d2516
