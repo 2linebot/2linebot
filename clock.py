901abd7f3e16a7c1876dc2259b5a3e6c6d987edf
Sean AP <66278738+2linebot@users.noreply.github.com>
2020-06-04 01:07:33 -0400

    Update clock.py

--- clock.py
+++ clock.py
@@ -4,6 +4,6 @@ sched=BlockingScheduler()
 
 @sched.scheduled_job('cron', hour=6-23, minute=0-59)
 def job():
-    print 'This job runs every minute from 6h00 to 23h59'
+    print('This job runs every minute from 6h00 to 23h59')
 
 sched.start()
