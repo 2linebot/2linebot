8006ff869bd2924a3cdfa25602464f384d9c64cc
Sean AP <66278738+2linebot@users.noreply.github.com>
2020-06-04 01:45:27 -0400

    Update 2line1.py

--- 2line1.py
+++ 2line1.py
@@ -10,30 +10,28 @@ sched=BlockingScheduler()
 def job():
     print('This job runs every 2 minutes')
 
-ckey=environ['ckey']
-csecret=environ['csecret']
-atoken=environ['atoken']
-asecret=environ['asecret']
+    ckey=environ['ckey']
+    csecret=environ['csecret']
+    atoken=environ['atoken']
+    asecret=environ['asecret']
 
-auth=tweepy.OAuthHandler(ckey, csecret)
-auth.set_access_token(atoken, asecret)
+    auth=tweepy.OAuthHandler(ckey, csecret)
+    auth.set_access_token(atoken, asecret)
 
-api=tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+    api=tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
-user=api.me()
+    user=api.me()
 
-search='"Line 2" from:TTCnotices'
-nrTweets=5
+    search='"Line 2" from:TTCnotices'
+    nrTweets=5
 
-for tweet in tweepy.Cursor(api.search, search).items(nrTweets):
-    try:
-        print('Alert RTd')
-        print(tweet.text)
-        tweet.retweet()
-        time.sleep(5)
-    except tweepy.TweepError as e:
-        print(e.reason)
-    except StopIteration:
-        break
-
-sched.start()
+    for tweet in tweepy.Cursor(api.search, search).items(nrTweets):
+        try:
+            print('Alert RTd')
+            print(tweet.text)
+         tweet.retweet()
+         time.sleep(5)
+        except tweepy.TweepError as e:
+            print(e.reason)
+        except StopIteration:
+           break
