2ced7304faee2e93efd2888a6dd1d9e81312102a
Sean AP <66278738+2linebot@users.noreply.github.com>
2020-06-04 16:29:25 -0400

    Add files via upload

---
+++ 2line1.py
@@ -0,0 +1,67 @@
+import tweepy
+import time
+import os
+from os import environ
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+sched=BlockingScheduler()
+
+@sched.scheduled_job('interval', minutes=2)
+def job():
+ckey=environ['ckey']
+csecret=environ['csecret']
+atoken=environ['atoken']
+asecret=environ['asecret']
+
+auth=tweepy.OAuthHandler(ckey, csecret)
+auth.set_access_token(atoken, asecret)
+
+api=tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+user=api.me()
+
+search='"Line 2" from:TTCnotices'
+nrTweets=5
+
+for tweet in tweepy.Cursor(api.search, search).items(nrTweets):
+    try:
+        print('Alert RTd')
+        print(tweet.text)
+        tweet.retweet()
+        time.sleep(5)
+    except tweepy.TweepError as e:
+        print(e.reason)
+    except StopIteration:
+        break
+sched.start()
+
+#import tweepy
+#import time
+#import os
+#from os import environ
+
+#ckey=environ['ckey']
+#csecret=environ['csecret']
+#atoken=environ['atoken']
+#asecret=environ['asecret']
+
+#auth=tweepy.OAuthHandler(ckey, csecret)
+#auth.set_access_token(atoken, asecret)
+
+#api=tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+#user=api.me()
+
+#search='"Line 2" from:TTCnotices'
+#nrTweets=5
+
+#for tweet in tweepy.Cursor(api.search, search).items(nrTweets):
+#    try:
+#        print('Alert RTd')
+#        print(tweet.text)
+#        tweet.retweet()
+#        time.sleep(5)
+#    except tweepy.TweepError as e:
+#        print(e.reason)
+#    except StopIteration:
+#        break
