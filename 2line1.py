a04a4a81750b6a5dd6c634bd6088c90d1628e953
Sean AP <66278738+2linebot@users.noreply.github.com>
2020-06-04 22:14:28 -0400

    Add files via upload

---
+++ 2line1.py
@@ -0,0 +1,36 @@
+import tweepy
+import time
+import os
+from os import environ
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+sched=BlockingScheduler()
+
+def job():
+    ckey=environ['ckey']
+    csecret=environ['csecret']
+    atoken=environ['atoken']
+    asecret=environ['asecret']
+
+    auth=tweepy.OAuthHandler(ckey, csecret)
+    auth.set_access_token(atoken, asecret)
+
+    api=tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+    user=api.me()
+
+    search='"Line 2" from:TTCnotices'
+    nrTweets=1
+
+    for tweet in tweepy.Cursor(api.search, search).items(nrTweets):
+        try:
+            print('Alert RTd')
+            print(tweet.text)
+            tweet.retweet()
+            time.sleep(1)
+        except tweepy.TweepError as e:
+            print(e.reason)
+        except StopIteration:
+            break
+sched.add_job(job, 'cron', hour='11-23,0-4', minute='*')
+sched.start()
