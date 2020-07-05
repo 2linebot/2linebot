de7a02f4e6964c66be77b0278728b35e7c44e9a7
Sean AP <66278738+2linebot@users.noreply.github.com>
2020-07-05 19:59:23 -0400

    Delete clock.py
    unnecessary files

--- clock.py
+++
@@ -1,9 +0,0 @@
-from apscheduler.schedulers.blocking import BlockingScheduler
-
-sched=BlockingScheduler()
-
-@sched.scheduled_job('interval', minutes=2)
-def job():
-    print('This job runs every 2 minutes')
-
-sched.start()
